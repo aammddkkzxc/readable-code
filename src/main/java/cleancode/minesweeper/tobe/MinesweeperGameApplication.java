ffd733cc9779bb8a29ded45c54cf0573c84f5c96
aammddkkzxc <ideal0427@naver.com>
2024-10-02 18:57:56 +0900

    feat: OCP: Open-Closed Principle 기존 코드의 변경 없이 기능 확장

--- src/main/java/cleancode/minesweeper/tobe/MinesweeperGameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/MinesweeperGameApplication.java
@@ -1,10 +1,17 @@
 package cleancode.minesweeper.tobe;
 
+import cleancode.minesweeper.tobe.gamelevel.Beginner;
+import cleancode.minesweeper.tobe.gamelevel.GameLevel;
+
 public class MinesweeperGameApplication {
 
     public static void main(String[] args) {
-        MineSweeper mineSweeper = new MineSweeper();
+
+        GameLevel gameLevel = new Beginner();
+
+        MineSweeper mineSweeper = new MineSweeper(gameLevel);
         mineSweeper.run();
+
     }
 
 }
